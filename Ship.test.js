61659b99a4e80651cf5e41b32834d4af0c888f94
Raihan Sharif <raihansharif92@gmail.com>
2025-05-26 17:16:02 +0100

    Ship testing done

--- Ship.test.js
+++ Ship.test.js
@@ -1,8 +1,29 @@
 import { Ship } from "./Ship";
 
 describe("ship tests", () => {
-  it("create ship of size 2", () => {
+  it("create ship of length 2", () => {
     let sh = new Ship(2);
     expect(sh).toBeInstanceOf(Ship);
+    expect(sh.length).toBe(2);
+  });
+
+  it("correctly registers hit", () => {
+    let sh = new Ship(2);
+    expect(sh.isSunk).toBe(false);
+    sh.hit();
+    expect(sh.numOfHits).toBe(1);
+    expect(sh.isSunk).toBe(false);
+    sh.hit();
+    expect(sh.numOfHits).toBe(2);
+    expect(sh.isSunk).toBe(true);
+  });
+
+  it("correctly registers if ship is sunk", () => {
+    let sh = new Ship(2);
+    expect(sh.isSunk).toBe(false);
+    sh.hit();
+    expect(sh.isSunk).toBe(false);
+    sh.hit();
+    expect(sh.isSunk).toBe(true);
   });
 });
